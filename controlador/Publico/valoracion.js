a326a8b5d3e0ee5c9b7836b24e28c0ff6cf93df2
LuisCruz991 <greninjaash1000@gmail.com>
2023-05-24 21:31:12 -0600

    bases de valoraciones

--- controlador/Publico/valoracion.js
+++ controlador/Publico/valoracion.js
@@ -76,25 +76,21 @@ async function fillTable(form = null) {
     if (JSON.status) {
         // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
         JSON.dataset.forEach(row => {
-            // Se establece un icono para el estado del producto.
-            (row.estado_valoracion) ? icon = '<i class="fa-solid fa-check"></i>' : icon = '<i class="fa-sharp fa-regular fa-ban"></i>';
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TBODY_ROWS.innerHTML += `
                 <tr>
-                    <td>${row.idvaloracion}</td>
-                    <td>${row.iddetalle_pedido}</td>
                     <td>${row.calificacion_producto}</td>
+                    <td>${row.fecha_comentario}</td>
                     <td>${row.nombre}</td>
                     <td>${row.titulo}</td>
                     <td>${row.resenia}</td>
-                    <td>${row.fecha_comentario}</td>
-                    <td>${icon}</td>
                     <td>
                     <a onclick="openUpdate(${row.idvaloracion})" data-bs-toggle="modal" data-bs-target="#save-modal" class="btn btn-primary tooltipped" data-tooltip="Actualizar">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-recycle" viewBox="0 0 16 16">
                     <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242-2.532-4.431zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24l2.552-4.467zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.498.498 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244l-1.716-3.004z"/>
                     </svg>
                     </a>
+                </td> 
                 </td> 
                 </tr>
             `;
@@ -150,11 +146,6 @@ async function openUpdate(id) {
         document.getElementById('titulo').value = JSON.dataset.titulo;
         document.getElementById('resenia').value = JSON.dataset.resenia;
         document.getElementById('fecha').value = JSON.dataset.fecha_comentario;
-        if (JSON.dataset.estado_valoracion) {
-            document.getElementById('estado').checked = true;
-        } else {
-            document.getElementById('estado').checked = false;
-        }
         // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
     } else {
         sweetAlert(2, JSON.exception, false);
