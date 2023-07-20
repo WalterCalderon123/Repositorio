23a6bb5d8883efb540416b6548906c01d6103b26
LuisCruz991 <greninjaash1000@gmail.com>
2023-07-20 12:01:50 -0600

    reporte

--- api/reports/dashboard/clientes_genero.php
+++ api/reports/dashboard/clientes_genero.php
@@ -5,50 +5,50 @@ require_once('../../helpers/report.php');
 // Se instancia la clase para crear el reporte.
 $pdf = new Report;
 // Se verifica si existe un valor para la categoría, de lo contrario se muestra un mensaje.
-if (isset($_GET['idestado_pedido'])) {
+if (isset($_GET['idgenero_cliente'])) {
     // Se incluyen las clases para la transferencia y acceso a datos.
-    require_once('../../entities/dto/pedido.php');
-    require_once('../../entities/dto/estado_pedido.php');
+    require_once('../../entities/dto/genero_cliente.php');
+    require_once('../../entities/dto/cliente.php');
     // Se instancian las entidades correspondientes.
-    $pedido = new Pedido;
-    $estado = new Estado;
+    $genero = new Genero;
+    $cliente = new Cliente;
     // Se establece el valor de la categoría, de lo contrario se muestra un mensaje.
-    if ($estado->setId($_GET['idestado_pedido']) && $producto->setCategoria($_GET['idestado_pedido'])) {
+    if ($genero->setId($_GET['idgenero_cliente']) && $cliente->setGenero($_GET['idgenero_cliente'])) {
         // Se verifica si la categoría existe, de lo contrario se muestra un mensaje.
-        if ($rowCategoria = $estado->readOne()) {
+        if ($rowGenero = $genero->readOne()) {
             // Se inicia el reporte con el encabezado del documento.
-            $pdf->startReport('Pedidos del estado ' . $rowCategoria['nombre_categoria']);
+            $pdf->startReport('Clientes de un genero' . $rowGenero['genero_cliente']);
             // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
-            if ($dataProductos = $producto->productosCategoria()) {
+            if ($dataClientes = $cliente->clientesGenero()) {
                 // Se establece un color de relleno para los encabezados.
                 $pdf->setFillColor(225);
                 // Se establece la fuente para los encabezados.
                 $pdf->setFont('Times', 'B', 11);
                 // Se imprimen las celdas con los encabezados.
                 $pdf->cell(126, 10, 'Nombre', 1, 0, 'C', 1);
-                $pdf->cell(30, 10, 'Precio (US$)', 1, 0, 'C', 1);
+                $pdf->cell(30, 10, 'DUI', 1, 0, 'C', 1);
                 $pdf->cell(30, 10, 'Estado', 1, 1, 'C', 1);
                 // Se establece la fuente para los datos de los productos.
                 $pdf->setFont('Times', '', 11);
                 // Se recorren los registros fila por fila.
-                foreach ($dataProductos as $rowProducto) {
-                    ($rowProducto['estado_producto']) ? $estado = 'Activo' : $estado = 'Inactivo';
+                foreach ($dataClientes as $rowCliente) {
+                    ($rowCliente['estado_producto']) ? $estado = 'Activo' : $estado = 'Inactivo';
                     // Se imprimen las celdas con los datos de los productos.
-                    $pdf->cell(126, 10, $pdf->encodeString($rowProducto['nombre_producto']), 1, 0);
-                    $pdf->cell(30, 10, $rowProducto['precio_producto'], 1, 0);
+                    $pdf->cell(126, 10, $pdf->encodeString($rowCliente['nombre_producto']), 1, 0);
+                    $pdf->cell(30, 10, $rowCliente['precio_producto'], 1, 0);
                     $pdf->cell(30, 10, $estado, 1, 1);
                 }
             } else {
-                $pdf->cell(0, 10, $pdf->encodeString('No hay productos para el estado'), 1, 1);
+                $pdf->cell(0, 10, $pdf->encodeString('No hay clientes para el genero'), 1, 1);
             }
             // Se llama implícitamente al método footer() y se envía el documento al navegador web.
-            $pdf->output('I', 'estado.pdf');
+            $pdf->output('I', 'genero.pdf');
         } else {
-            print('Estado inexistente');
+            print('Genero inexistente');
         }
     } else {
-        print('Estado incorrecto');
+        print('Genero incorrecto');
     }
 } else {
-    print('Debe seleccionar un Estado');
+    print('Debe seleccionar un Genero');
 }
